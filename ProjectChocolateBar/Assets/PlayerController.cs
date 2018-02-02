be67ca94f7c600d7db7430c2007c1acbe9b6c175
SunRobinDev (RobinS) <schrobin1505@gmail.com>
2018-02-02 13:42:58 +0100

    Create Repo
    Repo created with current State.

---
+++ ProjectChocolateBar/Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 6.0f;
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 180.0f;
+
+        transform.Translate(0, 0, z);
+        transform.Rotate(0, x, 0);
+
+    }
+}
