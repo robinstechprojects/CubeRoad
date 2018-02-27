9c0e76b464f41ebbf4af50f151df04ac967bd7d4
SunRobinDev (RobinS) <schrobin1505@gmail.com>
2018-02-27 17:21:31 +0100

    Implement Title Screen
    Implemented a kind of Title Screen

---
+++ ProjectChocolateBar/Assets/Scripts/Men.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Men : MonoBehaviour {
+
+	public void Level1(int level){
+		Application.LoadLevel(level);
+	}
+}
